66ec5db3aa9258cda93b0f434c5eddfb3c5e819b
Ritesh <124104044@nitkkr.ac.in>
2026-09-12 02:41:58 +0530

    Allow credentialed frontend API requests

--- backend/server.py
+++ backend/server.py
@@ -1935,7 +1935,7 @@ app.include_router(api)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=False,
+    allow_credentials=True,
     allow_origins=os.environ.get("CORS_ORIGINS", "*").split(","),
     allow_methods=["*"],
     allow_headers=["*"],
